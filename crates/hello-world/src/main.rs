53dd2fd211c21c6047291fcf54cdda4568342467
Ali Samji <ali@ybor.ai>
2025-09-27 09:16:38 -0500

    fix: Reset all.

--- crates/hello-world/src/main.rs
+++ crates/hello-world/src/main.rs
@@ -1,8 +1,2 @@
 fn main() {
-    println!("Fred sad!");
-    println!("Fred happy!");
-    println!("Fred dead!");
-    println!("Fred alive again!");
-    println!("Bread happy!");
-    println!("Bread & Fred!");
 }
