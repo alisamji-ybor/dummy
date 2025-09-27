e139943d16a5689fe25803cff004f2309a98ad99
Ali Samji <ali@ybor.ai>
2025-09-27 11:14:36 -0500

    fix: Smart Change.

--- crates/hello-world/src/main.rs
+++ crates/hello-world/src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
-    println!("Dummy 1!");
-    println!("Dummy 2!");
-    println!("Dummy 3!");
-    println!("Dummy 4!");
-    println!("Dummy 5!");
-    println!("Dummy 6!");
+    println!("Smarty 1!");
 }
