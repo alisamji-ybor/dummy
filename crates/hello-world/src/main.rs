e52b3a60f8a00576c6ae9d79f444b3b7d554c969
Ali Samji <ali@ybor.ai>
2025-09-29 17:00:43 -0500

    fix: Wow

--- crates/hello-world/src/main.rs
+++ crates/hello-world/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    println!("Smarty 1!");
-    println!("Smarty 2!");
-    println!("Smarty 3!");
-    println!("Smarty 4!");
-    println!("Smarty 5!");
-    println!("Smarty 10!");
-    println!("Smarty 11!");
-    println!("Smarty 455!");
-    println!("Smarty 12!");
+    println!("Work it 1!");
 }
